34036f55200f0b18be5b07e2d93c1b0577a2f489
DIO0550 <47467473+DIO0550@users.noreply.github.com>
2026-05-10 15:09:34 +0000

    ✨ [New Feature]: #155 graphics-state operators の barrel と register helper を追加
    cm / w / J / j / M の 5 handler を re-export し、`registerGraphicsStateOperators`
    helper で OperatorRegistry に一括登録する。タプル配列 + reduce + Result.flatMap
    による fail-fast 登録で、後続 operator カテゴリの登録パターンの雛形となる。
    
    Refs #155

---
+++ packages/core/src/content-stream/operators/graphics-state/index.ts
@@ -0,0 +1,45 @@
+import type { PdfError } from "../../../pdf/errors/index";
+import type { Result } from "../../../utils/result/index";
+import { flatMap, ok } from "../../../utils/result/index";
+import type { OperatorHandler } from "../../operator-registry/index";
+import { OperatorRegistry } from "../../operator-registry/index";
+import { cmHandler } from "./cm";
+import { lineCapHandler } from "./line-cap";
+import { lineJoinHandler } from "./line-join";
+import { lineWidthHandler } from "./line-width";
+import { miterLimitHandler } from "./miter-limit";
+
+export { cmHandler } from "./cm";
+export { lineCapHandler } from "./line-cap";
+export { lineJoinHandler } from "./line-join";
+export { lineWidthHandler } from "./line-width";
+export { miterLimitHandler } from "./miter-limit";
+
+const GRAPHICS_STATE_OPERATORS: ReadonlyArray<
+  readonly [string, OperatorHandler]
+> = [
+  ["cm", cmHandler],
+  ["w", lineWidthHandler],
+  ["J", lineCapHandler],
+  ["j", lineJoinHandler],
+  ["M", miterLimitHandler],
+];
+
+/**
+ * Graphics State operator (cm / w / J / j / M) を OperatorRegistry に
+ * 一括登録するヘルパ。
+ *
+ * fail-fast: いずれかの register が Err を返した時点で reduce 内 flatMap が
+ * 短絡し、後続 operator の register は呼ばれない。
+ *
+ * @param registry - 登録元 registry
+ * @returns 全 operator が登録された新しい registry、または重複登録時の PdfError
+ */
+export const registerGraphicsStateOperators = (
+  registry: OperatorRegistry,
+): Result<OperatorRegistry, PdfError> =>
+  GRAPHICS_STATE_OPERATORS.reduce<Result<OperatorRegistry, PdfError>>(
+    (acc, [name, handler]) =>
+      flatMap(acc, (r) => OperatorRegistry.register(r, name, handler)),
+    ok(registry),
+  );
